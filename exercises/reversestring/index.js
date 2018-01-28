24b4f8ee04400a84574c92b62981d4d48390e2d6
Vuong.Ngoc <vuong.ngoc@everymatrix.com>
2018-01-28 22:12:06 +0700

    reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,28 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
 
-module.exports = reverse;
+function reverse1(str) {
+    return str.split('').reduce((rev, char) => char + rev, '');
+}
+
+function reverse2(str) {
+    let reversed = '';
+    for (let char of str) {
+      reversed = char + reversed;
+    }
+    return reversed;
+}
+
+function reverse3(str) {
+    let reversed = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+      reversed += str[i];
+    }
+    return reversed;
+}
+
+module.exports = reverse3;
