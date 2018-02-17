a776c521c712676c3fd2b03e48b2b0963396ea8d
Vuong.Ngoc <vuong.ngoc@everymatrix.com>
2018-02-18 00:11:30 +0700

    steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,43 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//     for (let row = 0; row < n; row++) {
+//       let stair = '';
+//       for (let col = 0; col < n; col++) {
+//         if (col <= row) {
+//           stair += '#';
+//         } else {
+//           stair += ' ';
+//         }
+//       }
+//         console.log(stair);
+//     }
+//
+// }
+
+// function steps(n, row = 0, stair = '') {
+//     if (n === row) {
+//       return;
+//     }
+//     if (stair.length === n) {
+//       console.log(stair);
+//       steps(n, row + 1);
+//       return;
+//     }
+//     const add = stair.length <= row ? '#' : ' ';
+//     steps(n, row, stair + add);
+
+// }
+
+function steps(n) {
+  for (let i = 1; i <= n; i++) {
+    let stair = '';
+    stair += ('#'.repeat(i));
+    stair += (' '.repeat(n - i));
+    console.log(stair);
+  }
+}
+
 
 module.exports = steps;
