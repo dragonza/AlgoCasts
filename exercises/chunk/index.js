360c981795e2b8a2951a06a7f2c09cdc60fd4751
Vuong.Ngoc <vuong.ngoc@everymatrix.com>
2018-02-17 22:47:12 +0700

    update chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,39 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// function chunk(array, size) {
+//   const chunked_arr = [];
+//   const copied = [...array];
+//   let numOfChild = Math.ceil(copied.length / size);
+//   for (let i = 0; i < numOfChild; i++) {
+//     chunked_arr.push(copied.splice(0, size));
+//   }
+//   return chunked_arr;
+// }
+//
+//
+// function chunk(array, size) {
+//   const chunked_arr = [];
+//   let index = 0;
+//   while (index < array.length) {
+//     chunked_arr.push(array.slice(index, size + index));
+//     index += size;
+//   }
+//   return chunked_arr;
+// }
+
+
+function chunk(array, size) {
+    const chunked = [];
+    const last = chunked[chunked.length - 1];
+    for (let i = 0; i < array.length; i++) {
+      if (!last || last.length === size) {
+        chunked.push([array[i]])
+      } else {
+        last.push(array[i]);
+      }
+    }
+}
+
 
 module.exports = chunk;
