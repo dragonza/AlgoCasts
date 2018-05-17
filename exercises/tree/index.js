c67d4aa0a07d45e1dc78229a713bc29035c6ab71
Vuong.Ngoc <vuong.ngoc@everymatrix.com>
2018-05-17 21:07:00 +0700

    tree

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,43 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.children = [];
+    }
 
-class Tree {}
+    add(data) {
+        this.children.push(new Node(data));
+    }
+
+    remove(data) {
+        this.children = this.children.filter(node => !node.data === data);
+    }
+}
+
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+
+    traverseBF(func) {
+        const nodeArr = [this.root];
+        while (nodeArr.length) {
+            const node = nodeArr.shift();
+            nodeArr.push(...node.children);
+            func(node)
+        }
+    }
+
+    traverseDF(func) {
+        const arr = [this.root];
+        while (arr.length) {
+            const node = arr.shift();
+            arr.unshift(...node.children);
+            func(node)
+        }
+    }
+}
 
 module.exports = { Tree, Node };
